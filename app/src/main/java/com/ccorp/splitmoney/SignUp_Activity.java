8b0aaa96a9556378b68f62299ee29018c2ff33d6
kaustav07 <chatterjeekaustav11@gmail.com>
2016-07-29 02:46:24 +0530

    Including ButterKnife //kaustav

--- app/src/main/java/com/ccorp/splitmoney/SignUp_Activity.java
+++ app/src/main/java/com/ccorp/splitmoney/SignUp_Activity.java
@@ -4,17 +4,21 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.*;
+
+
 public class SignUp_Activity extends AppCompatActivity {
+
+    @InjectView(R.id.fullNameWrapper) TextInputLayout fullNameWrapper;
+    @InjectView(R.id.emailWrapper) TextInputLayout emailWrapper;
+    @InjectView(R.id.phoneWrapper) TextInputLayout phoneWrapper;
+    @InjectView(R.id.passwordWrapper) TextInputLayout passwordWrapper;
+    @InjectView(R.id.confirmPasswordWrapper) TextInputLayout confirmPasswordWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        final TextInputLayout fullNameWrapper = (TextInputLayout) findViewById(R.id.fullNameWrapper);
-        final TextInputLayout emailWrapper = (TextInputLayout) findViewById(R.id.emailWrapper);
-        final TextInputLayout phoneWrapper = (TextInputLayout) findViewById(R.id.phoneWrapper);
-        final TextInputLayout passwordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
-        final TextInputLayout confirmPasswordWrapper = (TextInputLayout) findViewById(R.id.confirmPasswordWrapper);
 
         fullNameWrapper.setHint("Full Name");
         emailWrapper.setHint("Email");
